15b7343c7c8c281a3199e7a10e1c3905e431843c
Wojtek Potocki <szkolenia@wojtekpotocki.pl>
2021-10-28 19:04:30 +0200

    radio buttons tests

---
+++ test/RadioButtonTests.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class RadioButtonTests extends BaseTest {
+
+    @Test
+    public void thereAreThreeRadioButtonInForm() {
+        driver.get("https://ultimateqa.com/simple-html-elements-for-automation/");
+        List<WebElement> radioButtons = driver.findElements(By.cssSelector("input[type='radio']"));
+
+        // sprawdzenie czy pierwszy radio jest zaznaczony na starcie
+        // System.out.println(radioButtons.get(0).isSelected());
+
+        Assert.assertEquals(radioButtons.size(), 3);
+    }
+
+    @Test(dependsOnMethods = {"thereAreThreeRadioButtonInForm"})
+    public void selectFemaleAndAssertOthersAreNotSelected() {
+        WebElement femaleRadioButton = driver.findElement(By.cssSelector("input[type='radio'][value='female']"));
+        femaleRadioButton.click();
+        List<WebElement> selectedRadioButtons = driver.findElements(By.cssSelector("input[type='radio']:checked"));
+
+        Assert.assertEquals(selectedRadioButtons.size(),1);
+    }
+}
